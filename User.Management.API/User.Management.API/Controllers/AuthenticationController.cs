2c61b2cbc75db629286c6e3c18e1e497a67dd3d5
install <install@SF-CPU-583>
2024-04-02 10:24:23 +0530

    Added Role Based Authentication And Authorization

---
+++ User.Management.API/User.Management.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using User.Management.API.Models;
+using User.Management.API.Models.Authentication.Login;
+using User.Management.API.Models.Authentication.SignUp;
+using User.Management.Service.Models;
+using User.Management.Service.Service;
+
+namespace User.Management.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        private readonly IEmailService _emailService;
+         
+        public AuthenticationController(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager, IConfiguration configuration, IEmailService emailService)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUser registerUser, string role)
+        {
+            var userExist = await _userManager.FindByEmailAsync(registerUser.Email);
+            if (userExist != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden,
+                    new Response { Status = "Error", Message = "User already exists!" });
+            }
+            IdentityUser user = new()
+            {
+                Email = registerUser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerUser.UserName
+            };
+
+            if(await _roleManager.RoleExistsAsync(role))
+            {
+                var result = await _userManager.CreateAsync(user, registerUser.Password);
+                if (!result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                         new Response { Status = "Error", Message = "User Failed to Create." });
+                }
+                await _userManager.AddToRoleAsync(user,role);
+
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var confirmationLink  = Url.Action(nameof(ConfirmEmail),"Authentication",new {token, email = user.Email});
+                var message = new Message(new string[] { user.Email! }, "Confirmation email link", confirmationLink);
+                _emailService.SendEmail(message);
+                return StatusCode(StatusCodes.Status200OK,
+                      new Response { Status = "Success", Message = $"User Created & Email Sent to {user.Email} Successfully" });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new Response { Status = "Error", Message = "This role Doesnot Exist." });
+            }
+            
+        }
+
+
+        //[HttpGet]
+        //public IActionResult TestEmail()
+        //{
+        //    var message =
+        //        new Message(new string[] { "jenishraiyani74@gmail.com" }, "Test", "<h1>Subscribe to my channel</h1>");
+        //    _emailService.SendEmail(message);
+        //    return StatusCode(StatusCodes.Status403Forbidden,
+        //         new Response { Status = "Error", Message = "User already exists!" });
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string token, string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if(user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user, token); 
+                if (result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status200OK,
+                   new Response { Status = "Success", Message = "Email Verified Successfully." });
+                }   
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                   new Response { Status = "Error", Message = "User Does Not exist!" });
+
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var user = await _userManager.FindByNameAsync(loginModel.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user,loginModel.Password))
+            {
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+                };
+                var userRoles = await _userManager.GetRolesAsync(user);
+                foreach(var role in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role,role));
+                }
+                var jwtToken = GetToken(authClaims);
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                    expiration = jwtToken.ValidTo
+                });
+            }
+            return Unauthorized();
+
+        }
+
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var token = new JwtSecurityToken(
+
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(1),
+                claims : authClaims,
+                signingCredentials : new SigningCredentials(authSigningKey,SecurityAlgorithms.HmacSha256)
+                );
+            return token;
+
+        }
+
+    }
+}
---
+++ User.Management.API/User.Management.API/Program.cs
@@ -0,0 +1,100 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using User.Management.API.Models;
+using User.Management.Service.Models;
+using User.Management.Service.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+// For Entity Frameowork
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ConnStr")));
+ 
+//For Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+// App Config for Required Email
+builder.Services.Configure<IdentityOptions>(options =>
+options.SignIn.RequireConfirmedEmail = true);
+
+//Adding Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = configuration["JWT:ValidAudience"],
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+    };
+});
+
+// Add Email Config
+var emailConfig = configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+builder.Services.AddSingleton(emailConfig);
+builder.Services.AddScoped<IEmailService, EmailService>();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Auth API", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Please enter valid token",
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new String[]{}
+        }
+
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
